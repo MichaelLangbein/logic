668d8675795b2de4ec42e437410a3c67cfbc1da9
Michael Langbein <michael_langbein@hotmail.de>
2023-03-02 20:30:17 +0100

    simple NN scaffold

--- nn/nn.py
+++ nn/nn.py
@@ -1,4 +1,4 @@
-from nodes import Node, Variable, MatMul, Sigmoid, gradient
+from nodes import Node, Sse, Variable, MatMul, Sigmoid, gradient
 import numpy as np
 
 
@@ -21,3 +21,53 @@ delta Wl = - alpha dE/dWl
 """
 
 
+class Layer:
+    def getParas(self):
+        pass
+    def eval(self, paras):
+        pass
+    def setInput(self, input):
+        pass
+    def getOutput(self):
+        pass
+    def update(self, error):
+        pass
+
+
+class NN:
+    def __init__(self, layers):
+
+        # 1: collect all parameters
+        paras = {
+            'input': None
+        }
+        for layer in layers:
+            layerParas = layer.getParas()
+            paras += layerParas
+        self.paras = paras
+
+        # 2: connect layers to each other
+        for i, layer in enumerate(layers):
+            if i == 0:
+                continue
+            prevLayer = layers[i - 1]
+            layer.setInput(prevLayer.getOutput())
+        self.layers = layers
+
+    def predict(self, input):
+        self.paras["input"] = input
+        lastLayer = self.layers[len(self.layers) - 1]
+        prediction = lastLayer.eval(self.paras)
+        return prediction
+    
+    def training(self, inputs, outputs):
+
+        lastLayer = self.layers[len(self.layers) - 1]
+        prediction = lastLayer.getOutput()
+        outputVariable = Variable("output")
+        error = Sse(outputVariable, prediction)
+        
+        for input, output in zip(inputs, outputs):
+            for layer in self.layers:
+                layer.update(error)
+        
