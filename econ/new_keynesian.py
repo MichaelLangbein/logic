1c6ac8199a4027dc26e57dfaaf4643fe44d466c5
Michael Langbein <michael_langbein@hotmail.de>
2024-05-31 12:09:50 +0200

    ..

--- econ/new_keynesian.py
+++ econ/new_keynesian.py
@@ -88,64 +88,71 @@ Fiscal policy seems to have a stronger effect on employment than monetary policy
 
 # %%
 
-def simulate2(
-        # IS curve
-        A: np.ndarray, 
-        alpha: float,
-        # Phillips curve
-        Yeq: np.ndarray,
-        k: float,
-        # Stabilizing interest
-        # Fed policy
-        gamma: float,
-        piTarget: np.ndarray
+def simulate(
+        T = 50,      # time steps
+        b = 2,       # wage bargaining: base demands
+        k = 0.5,     # wage bargaining: labor sensitive demands
+        c_a = 2,     # consumption, autonomous
+        c_y = 0.5,   # consumption, wage sensitive
+        a_a = 0.5,   # investment, autonomous
+        a_y = 0.5,   # investment, demand sensitive
+        a_r = 0.5,   # investment, interest-rate sensitive
+        t_a = 0.5,   # taxes, base interest-rate
+        t_y = 0.25,  # taxes, wage sensitive
+        p = 1,       # labor productivity
+        m = 0.1,     # gains markup
+        beta = 1,    # fed inflation/unemployment sensitivity,
+        PiT = 0.05,  # target inflation
+        G = 5,       # government spending
+        Yeq = 5,     # equilibrium production=goods-demand=employment*productivity && wage-demand=wage-supply, as determined by both goods- and factor-markets
 ):
-    """
-        IS curve: derived from firm's demands
-        $Y_t = A - \alpha r_{t-1}$
 
-        Phillips curve: derived from wage setting in factor market
-        $\pi = \pi_{-1} - k(L - L^{eq})$
+    A = (c_a + c_y * t_a + a_a + G) / (1 - c_y + c_y * t_y - a_y)
+    alpha = a_r / (1 - c_y + c_y * t_y - a_y)
+    rEq = (A - Yeq) / alpha
+    Leq = Yeq / p
 
-        Stabilizing interest rate (IS curve at Y^{eq})
-        $r^{eq} = (A - Y^{eq}) / \alpha$
+    # core variables
+    Y = np.ones(T)
+    r = np.ones(T)
+    Pi = np.ones(T)
 
-        Fed policy: loss function d/dL, substitute IS curve
-        $r_t = r^{eq} + \gamma (\pi - \pi^T)$
-    """
+    # derived
+    w_nom = np.ones(T)
+    L = np.ones(T)
+    P = np.ones(T)
 
-    T = 50
-    Y = np.zeros((T))
-    r = np.zeros((T))
-    pi = np.zeros((T))
-    Y[0] = 1
-    r[0] = 1.01
-    pi[0] = 0.01
+    # initial values: everything normal except goods-market requires more labor than the current wage-equilibrium
+    Y[0] = Yeq + 0.5   # shock: had to produce more because high demand on goods-market
+    L[0] = Y[0]/p      # Thus had to hire more people
+    r[0] = rEq         # r, Pi, w_nom have not yet adjusted
+    Pi[0] = PiT
+    w_nom[0] = b + k*(Yeq/p)
+    P[0] = (1 + m) * (w_nom[0] / p)
 
     for t in range(1, T):
-        # Productivity from IS curve
-        Y[t] = A[t] - alpha * r[t-1]
-        # Inflation from Phillips curve
-        pi[t] = pi[t-1] - k * (Y[t] - Yeq[t])
-        # Equilibrium-interest r_eq from IS curve
-        req = (A[t] - Yeq[t]) / alpha
-        # Fed sets real interest according to fed-policy
-        r[t] = req + gamma * (pi[t] - piTarget[t])
 
-    return Y, r, pi
+        # core variables
+        # IS curve: productivity(interest-rate)
+        Y[t] = A - alpha * r[t-1]
+        # Phillips curve: inflation(Labor)
+        Pi[t] = Pi[t-1] - k*(L[t-1] - Leq)
+        ## Central bank: rate(inflation)
+        cte = k / (alpha * (1 + (beta*k*k)/p))
+        r[t] = rEq - beta * cte * (Pi[t-1] - PiT)
+
+        # derived
+        L[t] = Y[t] / p
+        delta_w_nom = (Pi[t-1] - k*(L[t] - Leq)) * w_nom[t-1]
+        w_nom[t] = w_nom[t-1] + delta_w_nom
+        P[t] = (1 + m) * (w_nom[t] / p)
+
+    return Y, L, r, Pi, w_nom, P
+
 
 T = 50
-A = np.ones(T) * 10
-Yeq = np.ones(T) * 5
-YeqShock = np.ones(T) * 5
-YeqShock[20:] = 10
-piTarget = np.ones(T) * 0.05
-Y1, r1, pi1 = simulate2(A=A, alpha=0.1, Yeq=Yeq,      k=0.01, gamma=1, piTarget=piTarget)
-Y2, r2, pi2 = simulate2(A=A, alpha=0.1, Yeq=YeqShock, k=0.01, gamma=1, piTarget=piTarget)
+Ts = np.arange(0, T, 1)
+Y, L, r, Pi, w_nom, P = simulate(T=T)
 
-
-ts = np.linspace(0, T)
-
-plt.plot(ts, r1)
-plt.plot(ts, r2)
+plt.plot(Ts, r)
 # %%
